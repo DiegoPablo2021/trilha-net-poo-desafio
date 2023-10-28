b28c3a63cad936b8983da6b8148f7d091b9ca8ce
Diego Pablo <diegopmenezes@hotmail.com>
2023-10-28 11:46:48 -0300

    Desafio POO concluido com sucessogit add .

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(String numero, String modelo, String imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            
+        }
+
+        public override void InstalarAplicativo(string whatsapp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {whatsapp} no Iphone!" );
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia (numero: "111111111", modelo: "1", imei: "122333444455555", memoria: 512);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("==========================================");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone (numero: "222222222", modelo: "2", imei: "123456789101112", memoria: 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
